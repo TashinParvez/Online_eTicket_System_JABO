e834b5066f496313c6d61f8f6559a35d252a2893
Tashin Parvez <tashinparvez2002@gmail.com>
2022-12-21 22:43:48 +0600

    First Push

---
+++ src/main/java/com/mycompany/online_eticket_system_jabo/Online_eTicket_System_JABO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.online_eticket_system_jabo;
+
+/**
+ *
+ * @author Tashin Parvez
+ */
+public class Online_eTicket_System_JABO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
